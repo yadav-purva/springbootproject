c58fe5a49baeefbc786e1aeae2b002ed5a644bda
yadavpurva <yadav.purva2429@gmail.com>
2024-11-24 00:28:33 +0530

    Initial commit

---
+++ springbootproject/src/main/java/com/springboot/project/TestController.java
@@ -0,0 +1,14 @@
+package com.springboot.project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class TestController {
+    @RequestMapping("/test")
+    public String firstHandler(){
+        return "Just for Testing!!";
+    }
+}
